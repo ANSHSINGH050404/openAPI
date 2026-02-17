a95178de2db95efc964aca87c1b0ddf6943691c3
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2026-02-18 01:46:32 +0530

    feat: add API key controller with a create endpoint and stubs for other operations.

--- apps/primary-backend/src/controllers/apikey/apikey_controller.ts
+++ apps/primary-backend/src/controllers/apikey/apikey_controller.ts
@@ -1,14 +1,35 @@
 import type { Request, Response } from "express";
+import { ApikeyService } from "../../services/apikey";
 
+export const getApiKeys = (req: Request, res: Response) => {};
 
+export const createApiKey = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+    const userId = req.user?.id;
 
+    if (!userId || !name) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
 
-export const getApiKeys = (req:Request,res:Response) => {}
+    const result = await ApikeyService.createApikey(name, userId);
 
-export const createApiKey = (req:Request,res:Response) => {
-    
-}
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "Apikey created successfully",
+        id: result.id,
+        apikey: result.apikey,
+      });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
 
-export const disableApiKey = (req:Request,res:Response) => {}
+export const disableApiKey = (req: Request, res: Response) => {};
 
-export const deleteApiKey = (req:Request,res:Response) => {}
+export const deleteApiKey = (req: Request, res: Response) => {};
