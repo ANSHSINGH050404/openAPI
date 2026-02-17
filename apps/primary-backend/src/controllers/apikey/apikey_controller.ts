4a07897206388684549d77e5e277ce48a84f50a2
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2026-02-18 02:23:39 +0530

    feat: Add API key management routes and controller with functionality to get, create, and disable API keys.

--- apps/primary-backend/src/controllers/apikey/apikey_controller.ts
+++ apps/primary-backend/src/controllers/apikey/apikey_controller.ts
@@ -1,7 +1,36 @@
 import type { Request, Response } from "express";
 import { ApikeyService } from "../../services/apikey";
+import { prisma } from "db";
 
-export const getApiKeys = (req: Request, res: Response) => {};
+export const getApiKeys = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user?.id;
+    const apiKey = await prisma.apiKey.findMany({
+      where: {
+        userId: userId,
+      },    
+    });
+    return apiKey.map((key) => {
+      return {
+        id: key.id,
+        name: key.name,
+        creditsConsumed: key.creditsConsumed,
+        lastUsed: key.lastUsed,
+      };
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Apikeys fetched successfully",
+      data: apiKey,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
 
 export const createApiKey = async (req: Request, res: Response) => {
   try {
@@ -14,14 +43,12 @@ export const createApiKey = async (req: Request, res: Response) => {
 
     const result = await ApikeyService.createApikey(name, userId);
 
-    return res
-      .status(201)
-      .json({
-        success: true,
-        message: "Apikey created successfully",
-        id: result.id,
-        apikey: result.apikey,
-      });
+    return res.status(201).json({
+      success: true,
+      message: "Apikey created successfully",
+      id: result.id,
+      apikey: result.apikey,
+    });
   } catch (error) {
     console.log(error);
     return res
@@ -30,6 +57,58 @@ export const createApiKey = async (req: Request, res: Response) => {
   }
 };
 
-export const disableApiKey = (req: Request, res: Response) => {};
 
-export const deleteApiKey = (req: Request, res: Response) => {};
+
+export const disableApiKey = async (req: Request, res: Response) => {
+
+    const {userId} =req.user;
+    const {apiKeyId} = req.body;
+
+
+    if(!userId || !apiKeyId){
+        return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    const result = await prisma.apiKey.update({
+        where: {
+            id: apiKeyId,
+            userId:userId,
+        },
+        data:{
+            disabled:true,
+        }
+    });
+
+    return res.status(200).json({
+        success: true,
+        message: "Apikey disabled successfully",
+        data: result,
+    });
+
+    
+
+};
+
+export const deleteApiKey = async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const {userId} = req.user;
+
+    if(!userId || !id){
+        return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    const result = await prisma.apiKey.delete({
+        where: {
+            id: Number(id),
+            userId:userId,
+        },
+    });
+
+    return res.status(200).json({
+        success: true,
+        message: "Apikey deleted successfully",
+        data: result,
+    });
+
+    
+};
--- apps/primary-backend/src/routes/apikey_route.ts
+++ apps/primary-backend/src/routes/apikey_route.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth";
-import { createApiKey } from "../controllers/apikey/apikey_controller";
+import { createApiKey, getApiKeys, disableApiKey, deleteApiKey } from "../controllers/apikey/apikey_controller";
 
 const route = express.Router();
 
-route.get("/",authMiddleware)
+route.get("/",authMiddleware,getApiKeys)
 route.post("/",authMiddleware,createApiKey)
-route.post("/disable",authMiddleware)
-route.delete("/:id",authMiddleware)
+route.post("/disable",authMiddleware,disableApiKey)
+route.delete("/:id",authMiddleware,deleteApiKey)
 
 export default route;
